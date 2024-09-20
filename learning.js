228670aca2d361101cb75cb2105049305dc6ac8a
Jose Perez Jr <zenlaws02@gmail.com>
2024-09-20 18:59:02 +0800

    feat: reviewing how to write a file with nodejs

---
+++ learning.js
@@ -0,0 +1,15 @@
+import fs from 'fs';
+
+const text = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(text);
+
+// // Get the current timestamp
+const timestamp = Date.now();
+// // Convert the timestamp to a Date object
+const date = new Date(timestamp);
+// // Optionally, format the date to a readable string
+const readableDate = date.toLocaleString();
+
+const output = `Output: ${text}. \n Created on ${readableDate} `;
+fs.writeFileSync('./output/output.txt', output);
+console.log('File written successfully!');
